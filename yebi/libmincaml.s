64dbce85d74975b03ba219bdf493e2518b338c6c
Wataru Inariba <oinari17@gmail.com>
2014-10-18 02:39:17 +0900

    add comments on assembly libraries

--- yebi/libmincaml.s
+++ yebi/libmincaml.s
@@ -5,13 +5,13 @@ read_char:
     read    $1
     ret
 
-# write 1byte
+# <library> write 1byte
 .global print_char
 print_char:
     write   $1
     ret
 
-# read 4bytes (big endian)
+# <library> read 4bytes (big endian)
 .global read_int
 read_int:
 .global read_float
@@ -43,76 +43,76 @@ print_float:
     ret
 
 
-# $1: size / $2: initial value
+# <library> create_array(size, initial_value)
 .global create_array
 create_array:
-    ble     $1, $0, create_array_ret
-    mov     $3, [0x4000]
+    ble     $1, $0, array_L2    # if ($1 <= 0) return;
+    mov     $3, [0x4000]        # $3 = [0x4000];
     add     $4, $3, $1
-    mov     [0x4000], $4
-    sub     $4, $4, 1
-create_array_loop:
-    mov     [$3], $2
-    add     $3, $3, 1
-    ble     $3, $4, create_array_loop
-    sub     $1, $3, $1
-create_array_ret:
+    mov     [0x4000], $4        # [0x4000] = $3 + $1;
+    sub     $4, $4, 1           # $4 = $3 + $1 - 1;
+array_L1:                       # do {
+    mov     [$3], $2            #   [$3] = $2;
+    add     $3, $3, 1           #   $3 += 1;
+    ble     $3, $4, array_L1    # } while ($3 <= $4);
+    sub     $1, $3, $1          # return $3 - $1;
+array_L2:
     ret
 
 
-# int -> float
+# <library> float_of_int(int)
 itof_C1:
-    .int    0x4b000000      # float(2^23)
+    .int    0x4b000000          # float(2**23)
 itof_C2:
-    .int    0x56800000      # float(2^46)
+    .int    0x56800000          # float(2**46)
 .global float_of_int
 float_of_int:
-    bge     $1, $0, itof_abs
+    bge     $1, $0, itof_abs    # if ($1 < 0) {
     neg     $1, $1
-    call    itof_abs
-    fneg    $1, $1
+    call    itof_abs            #   $1 = itof_abs(-$1);
+    fneg    $1, $1              #   return -$1;
     ret
-itof_abs:
-    mov     $3, [itof_C1]
-    mov     $4, [itof_C2]
-    shr     $2, $1, 23
+itof_abs:                       # } else {
+    mov     $3, [itof_C1]       #   $3 = float(2**23);
+    mov     $4, [itof_C2]       #   $4 = float(2**46);
+    shr     $2, $1, 23          #   $2 = $1 >> 23;
     shl     $1, $1, 9
-    shr     $1, $1, 9
-    add     $1, $1, $3
-    add     $2, $2, $4
-    fneg    $3, $3
+    shr     $1, $1, 9           #   $1 = $1 & ((1 << 23) - 1);
+    add     $1, $1, $3          #   $1 += *(int*)&($3);
+    add     $2, $2, $4          #   $2 += *(int*)&($4);
+    fneg    $3, $3              #   // now $1, $2 have float type
     fneg    $4, $4
-    fadd    $1, $1, $3
-    fadd    $2, $2, $4
-    fadd    $1, $1, $2
-    ret
+    fadd    $1, $1, $3          #   $1 -= $3;
+    fadd    $2, $2, $4          #   $2 -= $4;
+    fadd    $1, $1, $2          #   return $1 + $2;
+    ret                         # }
 
 
-# float -> int
+# <library> int_of_float(float)
 ftoi_C1:
-    .int    0x00800000      # 2^23
+    .int    0x00800000          # 2**23
 .global int_of_float
 int_of_float:
-    bge     $1, $0, ftoi_abs
+    bge     $1, $0, ftoi_abs    # if ($1 < 0.0) {
     fneg    $1, $1
-    call    ftoi_abs
-    neg     $1, $1
+    call    ftoi_abs            #   $1 = ftoi_abs(-$1);
+    neg     $1, $1              #   return -$1;
     ret
-ftoi_abs:
+ftoi_abs:                       # } else {
     mov     $3, [ftoi_C1]
-    shr     $2, $1, 23
+    shr     $2, $1, 23          #   $2 = exponent($1);
     shl     $1, $1, 9
     shr     $1, $1, 9
-    add     $1, $1, $3
-    shift   $1, $1, $2 - 149
-    add     $1, $1, 1
-    shr     $1, $1, 1       # round towards infinity
-    ret
+    add     $1, $1, $3          #   $1 = fraction($1);
+    shift   $1, $1, $2 - 149    #   $1 >>= (127 - exponent + 22);
+    add     $1, $1, 1           #   // round towards infinity
+    shr     $1, $1, 1           #   return ($1 + 1) >> 1;
+    ret                         # }
 
 
-# floor function
+# <library> floor(x)
 floor_C1:
-    .int    0x4b000000      # float(2^23)
+    .int    0x4b000000          # float(2**23)
 floor_C2:
     .float  1
 floor_C3:
@@ -120,32 +120,32 @@ floor_C3:
 .global floor
 floor:
     mov     $2, [floor_C1]
-    bge     $1, $2, floor_ret
+    bge     $1, $2, floor_L4    # if ($1 >= 2**23) return $1;
     fadd    $3, $1, $2
-    ble     $3, $0, floor_ret
-    bge     $1, $0, floor_L2
-    fneg    $3, $1
-    fadd    $1, $2, $3
+    ble     $3, $0, floor_L4    # if ($1 <= -2**23) return $1;
+    bge     $1, $0, floor_L2    # if ($1 < 0) {
+    fneg    $3, $1              #   $3 = fabs($1);
+    fadd    $1, $2, $3          #   $1 = $3 + 2**23;
     fneg    $2, $2
-    fadd    $1, $1, $2
-    bge     $1, $3, floor_L1
+    fadd    $1, $1, $2          #   $1 -= 2**23;
+    bge     $1, $3, floor_L1    #   if ($1 < $3) {
     mov     $2, [floor_C2]
-    fadd    $1, $1, $2
-floor_L1:
-    fneg    $1, $1
+    fadd    $1, $1, $2          #     $1 += 1.0;
+floor_L1:                       #   }
+    fneg    $1, $1              #   return -$1;
     ret
-floor_L2:
-    fadd    $3, $1, $2
+floor_L2:                       # } else {
+    fadd    $3, $1, $2          #   $3 = $1 + 2**23
     fneg    $2, $2
-    fadd    $2, $2, $3
-    ble     $2, $1, floor_L3
+    fadd    $2, $2, $3          #   $2 = $3 - 2**23
+    ble     $2, $1, floor_L3    #   if ($2 > $1) {
     mov     $3, [floor_C3]
-    fadd    $1, $2, $3
+    fadd    $1, $2, $3          #     return $2 - 1.0;
     ret
-floor_L3:
-    mov     $1, $2
-floor_ret:
-    ret
+floor_L3:                       #   }
+    mov     $1, $2              #   return $2;
+floor_L4:
+    ret                         # }
 
 # another implementation of floor
 # floor2_C1:
@@ -177,21 +177,20 @@ floor_ret:
     # ret
 
 
-# division
+# <library> inverse
 .global finv
 finv:
     # TODO
     ret
 
 
-# square root
+# <library> square root
 .global sqrt
 sqrt:
     # TODO
     ret
 
 
-# sine / cosine function
 pi_q:
     .float  0.78539816
 pi_h:
@@ -201,38 +200,40 @@ pi_1:
 pi_2:
     .float  6.28318531
 
+# <library> sin(x)  [$5-9: reserved]
 .global sin
 sin:
-    shr     $2, $1, 31
+    shr     $2, $1, 31          # $2 = sign($1);
     shl     $1, $1, 1
     shr     $1, $1, 1
-    call    reduction
+    call    reduction           # $1 = reduction(fabs($1));
     mov     $3, [pi_1]
-    ble     $1, $3, sin_L1
-    add     $2, $2, 1
+    ble     $1, $3, sin_L1      # if ($1 > PI) {
+    add     $2, $2, 1           #   $2 = reverse($2);
     fneg    $4, $3
-    fadd    $1, $1, $4
-sin_L1:
+    fadd    $1, $1, $4          #   $1 -= PI;
+sin_L1:                         # }
     mov     $4, [pi_h]
-    ble     $1, $4, sin_L2
+    ble     $1, $4, sin_L2      # if ($1 > PI/2) {
     fneg    $1, $1
-    fadd    $1, $1, $3
-sin_L2:
+    fadd    $1, $1, $3          #   $1 = PI - $1;
+sin_L2:                         # }
     mov     $3, [pi_q]
-    ble     $1, $3, sin_L3
+    ble     $1, $3, sin_L3      # if ($1 > PI/4) {
     fneg    $1, $1
     fadd    $1, $1, $4
-    call    kernel_cos
+    call    kernel_cos          #   $1 = kernel_cos(PI/2 - $1);
     br      sin_L4
-sin_L3:
-    call    kernel_sin
-sin_L4:
-    beq     $1, $0, sin_L5
+sin_L3:                         # } else {
+    call    kernel_sin          #   $1 = kernel_sin($1);
+sin_L4:                         # }
+    beq     $1, $0, sin_L5      # if ($1 == 0.0) return $1;
     shl     $2, $2, 31
-    add     $1, $1, $2
+    add     $1, $1, $2          # sign($1) = $2;
 sin_L5:
-    ret
+    ret                         # return $1;
 
+# <library> cos(x)  [$5-9: reserved]
 .global cos
 cos:
     mov     $2, 0
@@ -266,24 +267,26 @@ cos_L4:
 cos_L5:
     ret
 
+# reduction(x)  [$1, $5-9: available]
 red_C1:
     .int    0x00800000
 reduction:
-    mov     $5, [pi_1]
+    mov     $5, [pi_1]          # $5 = PI;
     mov     $6, [pi_2]
     mov     $7, [red_C1]
-red_L1:
-    add     $5, $5, $7
-    bge     $1, $5, red_L1
-red_L2:
-    ble     $1, $5, red_L3
+red_L1:                         # do {
+    add     $5, $5, $7          #   $5 *= 2.0;
+    bge     $1, $5, red_L1      # } while ($1 >= $5);
+red_L2:                         # do {
+    ble     $1, $5, red_L3      #   if ($1 > $5) {
     fneg    $8, $5
-    fadd    $1, $1, $8
-red_L3:
-    sub     $5, $5, $7
-    bge     $5, $6, red_L2
-    ret
+    fadd    $1, $1, $8          #     $1 -= $5;
+red_L3:                         #   }
+    sub     $5, $5, $7          #   $5 /= 2.0;
+    bge     $5, $6, red_L2      # } while ($5 >= 2*PI);
+    ret                         # return $1;
 
+# kernel_sin(x)  [$1, $5-9: available]
 ksin_C0:
     .float   1.0
 ksin_C1:
@@ -307,6 +310,7 @@ kernel_sin:
     fmul    $1, $1, $6
     ret
 
+# kernel_cos(x)  [$1, $5-9: available]
 kcos_C0:
     .float   1.0
 kcos_C1:
@@ -330,7 +334,7 @@ kernel_cos:
     ret
 
 
-# arctangent function
+# <library> arctangent(x)
 .global atan
 atan:
     # TODO
