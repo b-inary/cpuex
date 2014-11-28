e3d023db3cc11a3841d9a9f6b0cc8ef64710e930
Wataru Inariba <oinari17@gmail.com>
2014-11-28 23:35:02 +0900

    implement sin and cos

--- gaia/uranuslib.ml
+++ gaia/uranuslib.ml
@@ -31,9 +31,42 @@ let float_of_int i = #itof i
 let int_of_float f = #ftoi f
 let floor f = #floor f
 
-(* TODO: trigonometric functions *)
-let sin x = x
-let cos x = x
-let atan x = x
+(* trigonometric functions *)
+let __kernel_sin x =
+  let sq = fsqr x in
+  x *. (1.0 +. sq *. (-0.16666668 +. sq *. (0.008332824 +. sq *. -0.00019587841)))
+let __kernel_cos x =
+  let sq = fsqr x in
+  1.0 +. sq *. (-0.5 +. sq *. (0.04166368 +. sq *. -0.0013695068))
+let __double x upper =
+  let d = 2.0 *. x in
+  if upper < d then x else __double d upper
+let __reduction_loop x p =
+  if p < 4. then x else
+  __reduction_loop (if x >= p then x -. p else x) (p *. 0.5)
+let __reduction x =
+  let pi2 = 6.2831853 in
+  __reduction_loop x (__double pi2 x)
+let sin x =
+  let piq = 0.78539816 in
+  let pih = 1.57079633 in
+  let pi  = 3.14159265 in
+  let x1 = __reduction (fabs x) in
+  let x2 = if x1 < pi  then x1 else x1 -. pi in
+  let x3 = if x2 < pih then x2 else pi -. x2 in
+  let abs = if x3 < piq then __kernel_sin x3 else __kernel_cos (pih -. x3) in
+  let sign = (0. <= x) = (x1 < pi) in (* true => positive *)
+  if sign then abs else fneg abs
+let cos x =
+  let piq = 0.78539816 in
+  let pih = 1.57079633 in
+  let pi  = 3.14159265 in
+  let x1 = __reduction (fabs x) in
+  let x2 = if x1 < pi  then x1 else x1 -. pi in
+  let x3 = if x2 < pih then x2 else pi -. x2 in
+  let abs = if x3 < piq then __kernel_cos x3 else __kernel_sin (pih -. x3) in
+  let sign = (x1 < pi) = (x2 < pih) in
+  if sign then abs else fneg abs
+let atan x = x (* TODO *)
 
 ;;
