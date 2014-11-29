f6e809de34c9f5f80b864603b7120340488cf379
Wataru Inariba <oinari17@gmail.com>
2014-11-29 17:21:45 +0900

    modify native.sh

--- gaia/native.sh
+++ gaia/native.sh
@@ -4,10 +4,13 @@ opt="opt-3.5"
 optopt="-S -O3 -std-compile-opts"
 llc="llc-3.5"
 
+datalayout="e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+triple="x86_64-pc-linux-gnu"
+
 name=${1%.*}
 
 ${llc} llvmlib.ll &&
-sed -e "s/^target datalayout.*$/target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"\ntarget triple = \"x86_64-pc-linux-gnu\"/" $1 > ${name}.native.ll &&
+sed -e "s/^target datalayout.*$/target datalayout = \"${datalayout}\"\ntarget triple = \"${triple}\"/" $1 > ${name}.native.ll &&
 ${opt} ${optopt} ${name}.native.ll -o ${name}.native.ll &&
 ${opt} ${optopt} ${name}.native.ll -o ${name}.native.ll &&  # optimize twice
 ${llc} ${name}.native.ll &&
